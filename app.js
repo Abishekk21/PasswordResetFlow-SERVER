c4268fc3ff86266a197a3c34bec6d0b4cf19a2dd
Abishekk21 <abishekkarunamoorthi@gmail.com>
2023-07-04 16:27:54 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-require("./db/conn");
+require("./database/conn");
 const router = require("./routes/router");
 const cors = require("cors");
 const cookiParser = require("cookie-parser");
